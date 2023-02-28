8f2d53d4f524b916842950c1fd1aa687f8909f3c
IrNaTik <ignitrudi@gmail.com>
2023-02-28 21:27:42 +0300

    Add CORS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('None');
+
+  useEffect(() => {
+    axios.get("http://localhost:8080")
+      .then((response) => {
+          console.log(response);
+          console.log(response.data)
+    })
+  }, [])
+
   return (
     <div className="App">
       
